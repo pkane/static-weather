90100d0dc90ff51a82776fa7a7714ef6f9beecb4
jesiah-hackinglife <jesiah@jesiah.net>
2014-03-13 11:08:54 -0400

    eh

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,8 +3,19 @@ angular.module('starter.controllers', [])
 
 // A simple controller that fetches a list of data from a service
 .controller('PetIndexCtrl', function($scope, PetService) {
+	$scope.myTitle = 'Pet Information';
   // "Pets" is a service returning mock data (services.js)
   $scope.pets = PetService.all();
+
+  $scope.leftButtons = [
+    {
+      type: 'button-positive',
+      content: '<i class="icon ion-navicon"></i>',
+      tap: function(e) {
+      }
+    }
+  ];
+
 })
 
 
